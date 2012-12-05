126f2eee8295b2e371560f906aefdca3ae8b3326
Axel Kohlmeyer <akohlmey@gmail.com>
2012-12-05 11:20:36 +0100

    improve code for better vectorization on suitable compilers. improve accuracy of exp2f()

--- exp2.c
+++ exp2.c
@@ -19,102 +19,136 @@
  *
  * the result becomes: exp2(x) = exp2(ipart) * exp2(fpart)
  */
-static const double fm_exp2_p[] = {
+static const double fm_exp2_r[] __attribute__ ((aligned(16))) = {
     2.30933477057345225087e-2,
+    1.00000000000000000000e0,
     2.02020656693165307700e1,
-    1.51390680115615096133e3
-};
-static const double fm_exp2_q[] = {
-/*  1.00000000000000000000e0, */
     2.33184211722314911771e2,
+    1.51390680115615096133e3,
     4.36821166879210612817e3
 };
 
-double fm_exp2(double x)
+double fm_exp2(const double x)
 {
     double   ipart, fpart, xx, px, qx;
     udi_t    epart;
 
-    /* handle exceptional arguments */
-    if (x == 0.0) 
-        return 1.0;
-    else if (x <= -1023.0)
-        return 0.0;
-    else if (x >= 1024.0)
-        return FM_INFINITY;
-
     ipart = __builtin_floor(x + 0.5);
     fpart = x - ipart;
     epart.f = 0.0;
-    epart.b.e = (unsigned int) (ipart + FM_DOUBLE_BIAS);
+    epart.b.e = ((unsigned int) ipart) + FM_DOUBLE_BIAS;
 
     xx = fpart*fpart;
 
-    px =         fm_exp2_p[0];
-    px = px*xx + fm_exp2_p[1];
-    qx =    xx + fm_exp2_q[0];
-    px = px*xx + fm_exp2_p[2];
-    qx = qx*xx + fm_exp2_q[1];
+    px =         fm_exp2_r[0];
+    qx =         fm_exp2_r[1];
+    px = px*xx + fm_exp2_r[2];
+    qx = qx*xx + fm_exp2_r[3];
+    px = px*xx + fm_exp2_r[4];
+    qx = qx*xx + fm_exp2_r[5];
+
     px = px * fpart;
 
     xx = 1.0 + 2.0*(px/(qx-px));
     return epart.f*xx;
 }
 
+
+#if 0
 /* optimizer friendly implementation of exp2f(x).
  *
  * strategy:
  *
  * split argument into an integer part and a fraction:
- * ipart = floor(x)+0.5;
+ * ipart = floorf(x)+0.5;
  * fpart = x - ipart;
  *
- * compute exp2(ipart) from setting the ieee754 exponent
- * compute exp2(fpart) using a pade' approximation for x in [-0.5;0.5[
+ * compute exp2f(ipart) from setting the ieee754 exponent
+ * compute exp2f(fpart) using a pade' approximation for x in [-0.5;0.5[
  *
- * the result becomes: exp2(x) = exp2(ipart) * exp2(fpart)
+ * the result becomes: exp2f(x) = exp2f(ipart) * exp2f(fpart)
  */
-static const float fm_exp2f_p[] = {
-    2.30933477057345225087e-2f,
-    2.02020656693165307700e1f,
-    1.51390680115615096133e3f
-};
-static const float fm_exp2f_q[] = {
-/*  1.00000000000000000000e0, */
+static const float fm_exp2f_r[] __attribute__ ((aligned(16))) = {
+    2.30933477057345225087e-2,
+    1.00000000000000000000e0,
+    2.02020656693165307700e1,
     2.33184211722314911771e2,
+    1.51390680115615096133e3,
     4.36821166879210612817e3
 };
 
-float fm_exp2f(float x)
+
+float fm_exp2f(const float x)
 {
     float ipart, fpart, xx, px, qx;
     ufi_t epart;
 
-    /* handle exceptional arguments */
-    if (x == 0.0f) 
-        return 1.0f;
-    else if (x <= -126.0f)
-        return 0.0;
-    else if (x >= 127.0)
-        return FM_INFINITYF;
-
     ipart = __builtin_floorf(x + 0.5f);
     fpart = x - ipart;
-    epart.f = 0.0f;
-    epart.b.e = (unsigned int) (ipart + FM_FLOAT_BIAS);
+    epart.u = 0U;
+    epart.b.e = ((unsigned int) ipart) + FM_FLOAT_BIAS;
 
     xx = fpart*fpart;
 
-    px =         fm_exp2f_p[0];
-    px = px*xx + fm_exp2f_p[1];
-    qx =    xx + fm_exp2f_q[0];
-    px = px*xx + fm_exp2f_p[2];
-    qx = qx*xx + fm_exp2f_q[1];
+    px =         fm_exp2f_r[0];
+    qx =         fm_exp2f_r[1];
+    px = px*xx + fm_exp2f_r[2];
+    qx = qx*xx + fm_exp2f_r[3];
+    px = px*xx + fm_exp2f_r[4];
+    qx = qx*xx + fm_exp2f_r[5];
+
     px = px * fpart;
 
     xx = 1.0f + 2.0f*(px/(qx-px));
     return epart.f*xx;
 }
+
+#else
+
+/* optimizer friendly implementation of exp2f(x).
+ *
+ * strategy:
+ *
+ * split argument into an integer part and a fraction:
+ * ipart = floorf(x)+0.5;
+ * fpart = x - ipart;
+ *
+ * compute exp2f(ipart) from setting the ieee754 exponent
+ * compute exp2f(fpart) using a polynomial approximation for x in [-0.5;0.5[
+ *
+ * the result becomes: exp2f(x) = exp2f(ipart) * exp2f(fpart)
+ */
+
+static const float fm_exp2f_p[] __attribute__ ((aligned(16))) = {
+    1.535336188319500e-004,
+    1.339887440266574e-003,
+    9.618437357674640e-003,
+    5.550332471162809e-002,
+    2.402264791363012e-001,
+    6.931472028550421e-001
+};
+
+float fm_exp2f(const float x)
+{
+    float ipart, fpart, xx, px, qx;
+    ufi_t epart;
+
+    ipart = __builtin_floorf(x + 0.5f);
+    fpart = x - ipart;
+    epart.u = 0U;
+    epart.b.e = ((unsigned int) ipart) + FM_FLOAT_BIAS;
+
+    px =            fm_exp2f_p[0];
+    px = px*fpart + fm_exp2f_p[1];
+    px = px*fpart + fm_exp2f_p[2];
+    px = px*fpart + fm_exp2f_p[3];
+    px = px*fpart + fm_exp2f_p[4];
+    px = px*fpart + fm_exp2f_p[5];
+    xx = px*fpart + 1.0f;
+
+    return epart.f*xx;
+}
+#endif
 
 /* 
  * Local Variables:
