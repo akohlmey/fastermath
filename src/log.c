d87fd24981d3e24e37e8f0361fa44bf8c07078c4
Axel Kohlmeyer <akohlmey@gmail.com>
2013-01-04 21:19:46 +0100

    add empty files for logarithms

---
+++ src/log.c
@@ -0,0 +1,20 @@
+/* faster versions of logarithms in single and double precision.
+ *
+ * Based on the Cephes math library 2.8
+ */
+
+#include "fastermath.h"
+#include "fm_internal.h"
+
+/* optimizer friendly implementation of log2(x). */
+
+
+/* 
+ * Local Variables:
+ * mode: c
+ * compile-command: "make"
+ * c-basic-offset: 4
+ * fill-column: 76 
+ * indent-tabs-mode: nil 
+ * End: 
+ */
