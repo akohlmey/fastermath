0bc64725ce4306b0959c402237be49ac5d25e0f3
Axel Kohlmeyer <akohlmey@gmail.com>
2013-01-04 20:55:55 +0100

    some cleanups and corrections for error estimates. start of log testing.

--- src/tester.c
+++ src/tester.c
@@ -33,7 +33,6 @@ int main(int argc, char **argv)
     double *xval, *res0, *res1, *res2, *res3;
     float  *xvalf, *res0f, *res1f, *res2f, *res3f;
     double xscale, err, sumerr, start;
-    double xscalef, errf, sumerrf;
     int num, rep, i, j;
     unsigned int seed;
 
@@ -49,8 +48,6 @@ int main(int argc, char **argv)
         seed = atoi(argv[2]);
     }
     srand(seed);
-    xscale = 1.0 / ((double) RAND_MAX);
-    xscalef = 1.0 / ((float) RAND_MAX);
 
 #define GRABMEM(var,type)                                          \
     posix_memalign((void **)&var, FM_DATA_ALIGN, num*sizeof(type)); \
@@ -70,8 +67,9 @@ int main(int argc, char **argv)
 
     puts("-------------------------\ntesting exponentiation functions");
     err = sumerr = 0.0;
+    xscale = 1.0 / ((double) RAND_MAX);
     start = wallclock(NULL);
-    /* random numbers distributed between -5.0 and 5.0 */ 
+    /* random numbers distributed between -10.0 and 10.0 */ 
     for (i=0; i < num; ++i) {
         double r1,r2,rsum;
         r1 = xscale * ((double) rand());
@@ -92,16 +90,16 @@ int main(int argc, char **argv)
 #define DOUBLE_ERROR(ref,res)                           \
     sumerr = 0.0;                                       \
     for (i=0; i < num; ++i) {                           \
-        sumerr += fabs((double)res[i]- (double)ref[i]); \
+        sumerr += fabs(((double)res[i]- (double)ref[i])/((double) ref[i])); \
     }                                                   \
-    printf("avgerr  %.6g\n", sumerr/((double) num*rep))
+    printf("avgerr  %.6g\n", sumerr/((double) num))
 
 #define FLOAT_ERROR(ref,res)                            \
     sumerrf = 0.0f;                                     \
     for (i=0; i < num; ++i) {                           \
-        sumerrf += fabsf(res[i]-ref[i]);                \
+        sumerrf += fabsf((res[i]-ref[i])/ref[i]);       \
     }                                                   \
-    printf("avgerr  %.6g\n", sumerrf/((double) num*rep))
+    printf("avgerr  %.6g\n", sumerrf/((double) num))
 
 #define RUN_LOOP(x,result,func,type)            \
     memset(result, 0, num*sizeof(type));        \
@@ -121,13 +119,6 @@ int main(int argc, char **argv)
 
     RUN_LOOP(xval,res2,fm_exp2,double);
     DOUBLE_ERROR(res0,res2);
-
-#if 0
-#ifdef __x86_64__
-    RUN_LOOP(xval,res3,amd_exp2,double);
-    DOUBLE_ERROR(res0,res3)
-#endif
-#endif
 
     RUN_LOOP(xval,res0,exp,double);
     printf("numreps %d\n", rep);
@@ -163,13 +154,6 @@ int main(int argc, char **argv)
     RUN_LOOP(xvalf,res2f,fm_exp2f,float);
     DOUBLE_ERROR(res0f,res2f);
 
-#if 0
-#ifdef __x86_64__
-    RUN_LOOP(xvalf,res3f,amd_exp2f,float);
-    DOUBLE_ERROR(res0f,res3f)
-#endif
-#endif
-
     RUN_LOOP(xvalf,res0f,expf,float);
     printf("numreps %d\n", rep);
 
@@ -194,49 +178,80 @@ int main(int argc, char **argv)
     RUN_LOOP(xvalf,res3f,fm_exp10f_alt,float);
     DOUBLE_ERROR(res0f,res3f);
 
+
+    puts("-------------------------\ntesting logarithm functions");
+    err = sumerr = 0.0;
+    xscale = 1.0 / ((double) RAND_MAX);
+    start = wallclock(NULL);
+    /* random numbers distributed between 2**-10.0 and 2**10.0 */ 
+    for (i=0; i < num; ++i) {
+        double r1,r2,rsum;
+        r1 = xscale * ((double) rand());
+        r2 = xscale * ((double) rand());
+        rsum = 0.5*(r1+r2);
+        xval[i] = exp2(20.0 * rsum  - 10.0);
+        xvalf[i] = (float) xval[i];
+        err += xval[i];
+        sumerr += xval[i]*xval[i];
+    }
+    err /= (double) num;
+    sumerr /= (double) num;
+    printf("time/set for %d x-values : %8.4gus\n", num, wallclock(&start)/num);    xscale = 1.0/(rep*num);
+
+    RUN_LOOP(xval,res0,log2,double);
+    printf("numreps %d\n", rep);
+
+    RUN_LOOP(xval,res1,__builtin_log2,double);
+    DOUBLE_ERROR(res0,res1);
+
+    RUN_LOOP(xvalf,res0f,log2f,float);
+    printf("numreps %d\n", rep);
+
+    RUN_LOOP(xvalf,res1f,__builtin_log2f,float);
+    DOUBLE_ERROR(res0f,res1f);
+
     return 0;
 
 #if 0
     /* special cases */
-    puts("testing special cases. libm exp2()");
-    printf("exp2(0.0)-1.0 = %.15g\n", exp2(0.0)-1.0);
-    printf("exp2(1.0)-2.0 = %.15g\n", exp2(1.0)-2.0);
-    printf("exp2(-0.5)-1.0/sqrt(2) = %.15g\n", exp2(-0.5)-1.0/sqrt(2.0));
-    printf("exp2(0.5)-sqrt(2) = %.15g\n", exp2(0.5)-sqrt(2.0));
-    printf("2.0*exp2(-0.5)-exp2(0.5) = %.15g\n", 2.0*exp2(-0.5)-exp2(0.5));
-    printf("2.0*exp2(0.0)-exp2(1.0) = %.15g\n", 2.0*exp2(0.0)-exp2(1.0));
-    printf("exp2(-1023.0) = %.15g\n", exp2(-1023.0));
-    printf("exp2( 1023.0) = %.15g\n", exp2( 1023.0));
-    printf("exp2(-1024.0) = %.15g\n", exp2(-1024.0));
-    printf("exp2( 1024.0) = %.15g\n", exp2( 1024.0));
-    printf("exp2(-2048.0) = %.15g\n", exp2(-2048.0));
-    printf("exp2( 2048.0) = %.15g\n", exp2( 2048.0));
 
-    puts("testing special cases. __builtin_exp2()");
-    printf("exp2(0.0)-1.0 = %.15g\n", __builtin_exp2(0.0)-1.0);
-    printf("exp2(1.0)-2.0 = %.15g\n", __builtin_exp2(1.0)-2.0);
-    printf("exp2(-0.5)-1.0/sqrt(2) = %.15g\n", __builtin_exp2(-0.5)-1.0/sqrt(2.0));
-    printf("exp2(0.5)-sqrt(2) = %.15g\n", __builtin_exp2(0.5)-sqrt(2.0));
-    printf("2.0*exp2(-0.5)-exp2(0.5) = %.15g\n", 2.0*__builtin_exp2(-0.5)-__builtin_exp2(0.5));
-    printf("2.0*exp2(0.0)-exp2(1.0) = %.15g\n", 2.0*__builtin_exp2(0.0)-__builtin_exp2(1.0));
-    printf("exp2(-1023.0) = %.15g\n", __builtin_exp2(-1023.0));
-    printf("exp2( 1023.0) = %.15g\n", __builtin_exp2( 1023.0));
-    printf("exp2(-1024.0) = %.15g\n", __builtin_exp2(-1024.0));
-    printf("exp2( 1024.0) = %.15g\n", __builtin_exp2( 1024.0));
-    printf("exp2(-2048.0) = %.15g\n", __builtin_exp2(-2048.0));
-    printf("exp2( 2048.0) = %.15g\n", __builtin_exp2( 2048.0));
 
-    puts("testing special cases. fm_exp2()");
-    printf("exp2(0.0)-1.0 = %.15g\n", fm_exp2(0.0)-1.0);
-    printf("exp2(1.0)-2.0 = %.15g\n", fm_exp2(1.0)-2.0);
-    printf("exp2(-0.5)-1.0/sqrt(2) = %.15g\n", fm_exp2(-0.5)-1.0/sqrt(2.0));
-    printf("exp2(0.5)-sqrt(2) = %.15g\n", fm_exp2(0.5)-sqrt(2.0));
-    printf("2.0*exp2(-0.5)-exp2(0.5) = %.15g\n", 2.0*fm_exp2(-0.5)-fm_exp2(0.5));
-    printf("2.0*exp2(0.0)-exp2(1.0) = %.15g\n", 2.0*fm_exp2(0.0)-fm_exp2(1.0));
-    printf("exp2(-1023.0) = %.15g\n", fm_exp2(-1023.0));
-    printf("exp2( 1023.0) = %.15g\n", fm_exp2( 1023.0));
-    printf("exp2(-2048.0) = %.15g\n", fm_exp2(-2048.0));
-    printf("exp2( 2048.0) = %.15g\n", fm_exp2( 2048.0));
+    puts("testing special cases. libm exp10() / exp10f()");
+    printf("exp10(0.0)-1.0 = %.15g\n", exp10(0.0)-1.0);
+    printf("exp10(1.0)-10.0 = %.15g\n", exp10(1.0)-10.0);
+    printf("exp10(-1.0)-0.1 = %.15g\n", exp10(-1.0)-0.1);
+    printf("exp10(10.0)-1e10 = %.15g\n", exp10(10.0)-1e10);
+
+    printf("exp10f(0.0f)-1.0f = %.15g\n", exp10f(0.0f)-1.0f);
+    printf("exp10f(1.0f)-10.0f = %.15g\n", exp10f(1.0f)-10.0f);
+    printf("exp10(-1.0f)-0.1f = %.15g\n", exp10f(-1.0f)-0.1f);
+    printf("exp10(10.0f)-1e10f = %.15g\n", exp10f(10.0f)-1e10f);
+    printf("exp10(10.0f) = %.15g\n", exp10f(10.0f));
+
+    puts("testing special cases. libm fm_exp10() / fm_exp10f()");
+    printf("exp10(0.0)-1.0 = %.15g\n", fm_exp10(0.0)-1.0);
+    printf("exp10(1.0)-10.0 = %.15g\n", fm_exp10(1.0)-10.0);
+    printf("exp10(-1.0)-0.1 = %.15g\n", fm_exp10(-1.0)-0.1);
+    printf("exp10(10.0)-1e10 = %.15g\n", fm_exp10(10.0)-1e10);
+
+    printf("exp10f(0.0f)-1.0f = %.15g\n", fm_exp10f(0.0f)-1.0f);
+    printf("exp10f(1.0f)-10.0f = %.15g\n", fm_exp10f(1.0f)-10.0f);
+    printf("exp10(-1.0f)-0.1f = %.15g\n", fm_exp10f(-1.0f)-0.1f);
+    printf("exp10(10.0f)-1e10f = %.15g\n", fm_exp10f(10.0f)-1e10f);
+    printf("exp10(10.0f) = %.15g\n", fm_exp10f(10.0f));
+
+    puts("testing special cases. libm fm_exp10_alt() / fm_exp10f_alt()");
+    printf("exp10(0.0)-1.0 = %.15g\n", fm_exp10_alt(0.0)-1.0);
+    printf("exp10(1.0)-10.0 = %.15g\n", fm_exp10_alt(1.0)-10.0);
+    printf("exp10(-1.0)-0.1 = %.15g\n", fm_exp10_alt(-1.0)-0.1);
+    printf("exp10(10.0)-1e10 = %.15g\n", fm_exp10_alt(10.0)-1e10);
+    printf("exp10(10.0f) = %.15g\n", exp10f(10.0f));
+
+    printf("exp10f(0.0f)-1.0f = %.15g\n", fm_exp10f_alt(0.0f)-1.0f);
+    printf("exp10f(1.0f)-10.0f = %.15g\n", fm_exp10f_alt(1.0f)-10.0f);
+    printf("exp10(-1.0)-0.1f = %.15g\n", fm_exp10f_alt(-1.0f)-0.1f);
+    printf("exp10(10.0f)-1e10f = %.15g\n", fm_exp10f_alt(10.0f)-1e10f);
+
 #endif
 
 }
